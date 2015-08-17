e9c6a46923fcb772176dc924f0e4b15c46e9d671
Simon Reeves <simon.j.reeves@gmail.com>
2015-08-17 07:59:39 +0100

    sin wave demo

--- client/base.js
+++ client/base.js
@@ -1,9 +1,9 @@
 var app = angular.module("svg");
 
-app.controller("baseCtrl",["$scope",function($scope){
+app.controller("baseCtrl",["$scope","$timeout",function($scope,$timeout){
 	$scope.message = "controller init message";
 	
-	$scope.graph = {'width':265,'height':134};
+	$scope.size = {'width':265,'height':400};
 	
 	$scope.circles =[];
 	var c1 = {x:100,y:100,r:20};
@@ -12,7 +12,25 @@ app.controller("baseCtrl",["$scope",function($scope){
 	$scope.paths = [];
 	
 	$scope.addpath=function(){
-	$scope.paths.push("M150 0 L75 200 L225 200 Z");
+		$scope.paths.push("M150 0 L75 200 L225 200 Z");
 	};
 	
+	$scope.populateGraph = function(offset){
+		$scope.graph = "M0 100 ";
+		for ( i = 0 ; i < 2*Math.PI; i=i+Math.PI/16){
+			var y = Math.sin(i+offset);
+			var x = (i/(2*Math.PI))*100
+			
+			$scope.graph += "L "+x+" "+(100-(y*100)) + " ";
+		}
+		$scope.paths = [];
+		$scope.paths.push($scope.graph);
+	};
+	$scope.offset = 0;
+	$scope.moveGraph = function(){
+		$scope.offset= $scope.offset + (Math.PI/16);
+		$scope.populateGraph($scope.offset);
+		$timeout($scope.moveGraph,60);
+	};
+	$timeout($scope.moveGraph,60);
 }]);
